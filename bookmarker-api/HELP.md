f21305fa8999f6e132470c7b6eae4ea4642e4417
khekrn <kishorenayark@gmail.com>
2023-01-20 18:57:45 +0530

    Initial Commit, Get bookmark api is working as expected

---
+++ bookmarker-api/HELP.md
@@ -0,0 +1,28 @@
+# Getting Started
+
+### Reference Documentation
+For further reference, please consider the following sections:
+
+* [Official Apache Maven documentation](https://maven.apache.org/guides/index.html)
+* [Spring Boot Maven Plugin Reference Guide](https://docs.spring.io/spring-boot/docs/3.0.1/maven-plugin/reference/html/)
+* [Create an OCI image](https://docs.spring.io/spring-boot/docs/3.0.1/maven-plugin/reference/html/#build-image)
+* [Testcontainers Postgres Module Reference Guide](https://www.testcontainers.org/modules/databases/postgres/)
+* [Spring Configuration Processor](https://docs.spring.io/spring-boot/docs/3.0.1/reference/htmlsingle/#appendix.configuration-metadata.annotation-processor)
+* [Spring Boot DevTools](https://docs.spring.io/spring-boot/docs/3.0.1/reference/htmlsingle/#using.devtools)
+* [Spring Web](https://docs.spring.io/spring-boot/docs/3.0.1/reference/htmlsingle/#web)
+* [Spring Boot Actuator](https://docs.spring.io/spring-boot/docs/3.0.1/reference/htmlsingle/#actuator)
+* [Validation](https://docs.spring.io/spring-boot/docs/3.0.1/reference/htmlsingle/#io.validation)
+* [Spring Data JPA](https://docs.spring.io/spring-boot/docs/3.0.1/reference/htmlsingle/#data.sql.jpa-and-spring-data)
+* [Testcontainers](https://www.testcontainers.org/)
+* [Flyway Migration](https://docs.spring.io/spring-boot/docs/3.0.1/reference/htmlsingle/#howto.data-initialization.migration-tool.flyway)
+
+### Guides
+The following guides illustrate how to use some features concretely:
+
+* [Building a RESTful Web Service](https://spring.io/guides/gs/rest-service/)
+* [Serving Web Content with Spring MVC](https://spring.io/guides/gs/serving-web-content/)
+* [Building REST services with Spring](https://spring.io/guides/tutorials/rest/)
+* [Building a RESTful Web Service with Spring Boot Actuator](https://spring.io/guides/gs/actuator-service/)
+* [Validation](https://spring.io/guides/gs/validating-form-input/)
+* [Accessing Data with JPA](https://spring.io/guides/gs/accessing-data-jpa/)
+
---
+++ bookmarker-api/src/main/java/com/khekrn/bookmarker/DataInitializer.java
@@ -0,0 +1,39 @@
+package com.khekrn.bookmarker;
+
+import com.khekrn.bookmarker.domain.Bookmark;
+import com.khekrn.bookmarker.domain.BookmarkRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+import java.time.Instant;
+import java.util.ArrayList;
+
+@Component
+@RequiredArgsConstructor
+public class DataInitializer implements CommandLineRunner {
+
+    private final BookmarkRepository bookmarkRepository;
+
+    @Override
+    public void run(String... args) {
+        var bookmarks = new ArrayList<Bookmark>();
+        bookmarks.add(new Bookmark(null, "SivaLabs", "https://sivalabs.in", Instant.now()));
+        bookmarks.add(new Bookmark(null, "SpringBlog", "https://spring.io/blog", Instant.now()));
+        bookmarks.add(new Bookmark(null, "Quarkus", "https://quarkus.io/", Instant.now()));
+        bookmarks.add(new Bookmark(null, "Micronaut", "https://micronaut.io/", Instant.now()));
+        bookmarks.add(new Bookmark(null, "JOOQ", "https://www.jooq.org/", Instant.now()));
+        bookmarks.add(new Bookmark(null, "VladMihalcea", "https://vladmihalcea.com", Instant.now()));
+        bookmarks.add(new Bookmark(null, "Thoughts On Java", "https://thorben-janssen.com/", Instant.now()));
+        bookmarks.add(new Bookmark(null, "DZone", "https://dzone.com/", Instant.now()));
+        bookmarks.add(new Bookmark(null, "DevOpsBookmarks", "http://www.devopsbookmarks.com/", Instant.now()));
+        bookmarks.add(new Bookmark(null, "Kubernetes docs", "https://kubernetes.io/docs/home/", Instant.now()));
+        bookmarks.add(new Bookmark(null, "Expressjs", "https://expressjs.com/", Instant.now()));
+        bookmarks.add(new Bookmark(null, "Marcobehler", "https://www.marcobehler.com", Instant.now()));
+        bookmarks.add(new Bookmark(null, "baeldung", "https://www.baeldung.com", Instant.now()));
+        bookmarks.add(new Bookmark(null, "devglan", "https://www.devglan.com", Instant.now()));
+        bookmarks.add(new Bookmark(null, "linuxize", "https://linuxize.com", Instant.now()));
+
+        bookmarkRepository.saveAll(bookmarks);
+    }
+}
