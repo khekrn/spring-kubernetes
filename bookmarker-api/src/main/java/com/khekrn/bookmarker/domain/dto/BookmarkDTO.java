097f2821919ded09298b5939b59e12bf9e07bdc4
khekrn <kishorenayark@gmail.com>
2024-01-06 18:02:51 +0530

    Completing part 4, Using record for DTO and understanding the need for JPA projections

--- bookmarker-api/src/main/java/com/khekrn/bookmarker/domain/dto/BookmarkDTO.java
+++ bookmarker-api/src/main/java/com/khekrn/bookmarker/domain/dto/BookmarkDTO.java
@@ -1,16 +1,5 @@
 package com.khekrn.bookmarker.domain.dto;
 
-import lombok.*;
-
 import java.time.Instant;
 
-@Getter
-@Setter
-@AllArgsConstructor
-public class BookmarkDTO {
-
-    private Long id;
-    private String title;
-    private String url;
-    Instant createdAt;
-}
+public record BookmarkDTO(Long id, String title, String url, Instant createdAt) { }
